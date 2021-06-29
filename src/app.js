9619712664dc0580f8ed097b8c8681a2968c6831
lucasfranchini <lucasfranchini.a@gmail.com>
2021-06-28 22:48:54 -0300

    starting products route

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import connection from './database';
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+
+app.get('/products', async (req,res)=>{
+    try{
+        const limit = req.query.limit || 20;
+        const offset = req.query.offset || 0; 
+        const result = await connection.query(`
+        SELECT *
+        FROM products
+        LIMIT $1
+        OFFSET $2
+        `,[limit,offset])
+        res.send(result.rows)
+    }
+    catch (e){
+        console.log(e);
+        res.sendStatus(500)
+    }
+})
--- src/database.js
+++ src/database.js
@@ -15,7 +15,7 @@ export default connection;
 /* 
 
 CREATE TABLE users (id SERIAL, email TEXT, password TEXT);
-CREATE TABLE products (id SERIAL, name TEXT,description TEXT);
+CREATE TABLE products (id SERIAL, name TEXT,description TEXT,type text,price integer);
 create TABLE stock (id SERIAL, "productId" INTEGER, qtd INTEGER);
 create TABLE sales (id SERIAL, "userId" INTEGER, "orderId" INTEGER, "productId" integer,qtd INTEGER);
 create TABLE orders (id SERIAL, "userId" INTEGER ,adress TEXT, "paymentType" TEXT);
