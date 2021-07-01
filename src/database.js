04f460d4253bcb5edb5594be76d19c937f3fb4b0
lucasfranchini <lucasfranchini.a@gmail.com>
2021-07-01 13:34:15 -0300

    changing database for environment variables

--- src/database.js
+++ src/database.js
@@ -3,11 +3,11 @@ import pg from "pg";
 const { Pool } = pg;
 
 const connection = new Pool({
-  user: "postgres",
-  password: "123456",
-  host: "localhost",
-  port: 5432,
-  database: process.env.NODE_ENV === "test" ? "vetmed_test" : "vetmed",
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	port: process.env.DB_PORT,
+  database: process.env.NODE_ENV === "test" ? "vetmed_test" : process.env.DB_DATABASE,
 });
 
 export default connection;
--- tests/categories.test.js
+++ tests/categories.test.js
@@ -1,3 +1,4 @@
+import '../src/setup.js';
 import supertest from "supertest";
 import app from "../src/app";
 import connection from "../src/database";
