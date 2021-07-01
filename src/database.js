88377d8835e03a912271222fb6b4b6218eca22a7
lucasfranchini <lucasfranchini.a@gmail.com>
2021-07-01 15:00:10 -0300

    prepare for deploy

--- src/database.js
+++ src/database.js
@@ -8,16 +8,9 @@ const connection = new Pool({
 	password: process.env.DB_PASSWORD,
 	port: process.env.DB_PORT,
   database: process.env.NODE_ENV === "test" ? "vetmed_test" : process.env.DB_DATABASE,
+  ssl: {
+    rejectUnauthorized: false
+}
 });
 
 export default connection;
-
-/* 
-
-CREATE TABLE users (id SERIAL, email TEXT, password TEXT);
-CREATE TABLE products (id SERIAL, name TEXT,description TEXT,type text,price integer);
-create TABLE stock (id SERIAL, "productId" INTEGER, qtd INTEGER);
-create TABLE sales (id SERIAL, "userId" INTEGER, "orderId" INTEGER, "productId" integer,qtd INTEGER);
-create TABLE orders (id SERIAL, "userId" INTEGER ,adress TEXT, "paymentType" TEXT);
-
-*/
