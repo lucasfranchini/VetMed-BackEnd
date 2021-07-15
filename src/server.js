b2d661a4507a200c8e4ae2b6259011e7fa7086b2
leleo <celso@respondeai.com.br>
2021-07-15 15:48:26 -0300

    code review

--- src/server.js
+++
@@ -1,6 +0,0 @@
-import './setup.js'; 
-import app from "./app.js";
-
-const port = process.env.PORT;
-const nodeEnv = process.env.NODE_ENV;
-app.listen(port, () => console.log(`Server is Running on port ${port} with NODE_ENV ${nodeEnv}`));
--- tests/flushDatabase.js
+++
@@ -1,8 +0,0 @@
-export default async function flushDatabase(connection) {
-  await connection.query(`DELETE FROM orders`);
-  await connection.query(`DELETE FROM products`);
-  await connection.query(`DELETE FROM sales`);
-  await connection.query(`DELETE FROM sessions`);
-  await connection.query(`DELETE FROM stock`);
-  await connection.query(`DELETE FROM users`);
-}
